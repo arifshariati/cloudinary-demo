2d689b5add293056043e74f367e7fe3f8d5886b0
Arif Shariati <mohammad.arif.fast@hotmail.com>
2024-05-09 08:53:02 +0200

    added image actions

--- src/lib/actions/image.actions.ts
+++ src/lib/actions/image.actions.ts
@@ -1,56 +1,181 @@
 "use server";
 
-import { redirect } from "next/navigation";
-import Stripe from "stripe";
-import { handleError } from "../utils";
+import { revalidatePath } from "next/cache";
 import { connectToDatabase } from "../database/mongoose";
-import Transaction from "../database/models/transaction.model";
-import { updateCredits } from "./user.actions";
+import { handleError } from "../utils";
+import User from "../database/models/user.model";
+import Image from "../database/models/image.model";
+import { redirect } from "next/navigation";
 
-export async function checkoutCredits(transaction: CheckoutTransactionParams) {
-  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+import { v2 as cloudinary } from 'cloudinary'
 
-  const amount = Number(transaction.amount) * 100;
+const populateUser = (query: any) => query.populate({
+  path: 'author',
+  model: User,
+  select: '_id firstName lastName clerkId'
+})
 
-  const session = await stripe.checkout.sessions.create({
-    line_items: [
-      {
-        price_data: {
-          currency: "usd",
-          unit_amount: amount,
-          product_data: {
-            name: transaction.plan,
-          },
-        },
-        quantity: 1,
-      },
-    ],
-    metadata: {
-      plan: transaction.plan,
-      credits: transaction.credits,
-      buyerId: transaction.buyerId,
-    },
-    mode: "payment",
-    success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/profile`,
-    cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/`,
-  });
-
-  redirect(session.url!);
-}
-
-export async function createTransaction(transaction: CreateTransactionParams) {
+// ADD IMAGE
+export async function addImage({ image, userId, path }: AddImageParams) {
   try {
     await connectToDatabase();
 
-    // Create a new transaction with a buyerId
-    const newTransaction = await Transaction.create({
-      ...transaction,
-      buyer: transaction.buyerId,
-    });
+    const author = await User.findById(userId);
 
-    await updateCredits(transaction.buyerId, transaction.credits);
+    if (!author) {
+      throw new Error("User not found");
+    }
 
-    return JSON.parse(JSON.stringify(newTransaction));
+    const newImage = await Image.create({
+      ...image,
+      author: author._id,
+    })
+
+    revalidatePath(path);
+
+    return JSON.parse(JSON.stringify(newImage));
+  } catch (error) {
+    handleError(error)
+  }
+}
+
+// UPDATE IMAGE
+export async function updateImage({ image, userId, path }: UpdateImageParams) {
+  try {
+    await connectToDatabase();
+
+    const imageToUpdate = await Image.findById(image._id);
+
+    if (!imageToUpdate || imageToUpdate.author.toHexString() !== userId) {
+      throw new Error("Unauthorized or image not found");
+    }
+
+    const updatedImage = await Image.findByIdAndUpdate(
+      imageToUpdate._id,
+      image,
+      { new: true }
+    )
+
+    revalidatePath(path);
+
+    return JSON.parse(JSON.stringify(updatedImage));
+  } catch (error) {
+    handleError(error)
+  }
+}
+
+// DELETE IMAGE
+export async function deleteImage(imageId: string) {
+  try {
+    await connectToDatabase();
+
+    await Image.findByIdAndDelete(imageId);
+  } catch (error) {
+    handleError(error)
+  } finally{
+    redirect('/')
+  }
+}
+
+// GET IMAGE
+export async function getImageById(imageId: string) {
+  try {
+    await connectToDatabase();
+
+    const image = await populateUser(Image.findById(imageId));
+
+    if(!image) throw new Error("Image not found");
+
+    return JSON.parse(JSON.stringify(image));
+  } catch (error) {
+    handleError(error)
+  }
+}
+
+// GET IMAGES
+export async function getAllImages({ limit = 9, page = 1, searchQuery = '' }: {
+  limit?: number;
+  page: number;
+  searchQuery?: string;
+}) {
+  try {
+    await connectToDatabase();
+
+    cloudinary.config({
+      cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+      api_key: process.env.CLOUDINARY_API_KEY,
+      api_secret: process.env.CLOUDINARY_API_SECRET,
+      secure: true,
+    })
+
+    let expression = 'folder=imaginify';
+
+    if (searchQuery) {
+      expression += ` AND ${searchQuery}`
+    }
+
+    const { resources } = await cloudinary.search
+      .expression(expression)
+      .execute();
+
+    const resourceIds = resources.map((resource: any) => resource.public_id);
+
+    let query = {};
+
+    if(searchQuery) {
+      query = {
+        publicId: {
+          $in: resourceIds
+        }
+      }
+    }
+
+    const skipAmount = (Number(page) -1) * limit;
+
+    const images = await populateUser(Image.find(query))
+      .sort({ updatedAt: -1 })
+      .skip(skipAmount)
+      .limit(limit);
+    
+    const totalImages = await Image.find(query).countDocuments();
+    const savedImages = await Image.find().countDocuments();
+
+    return {
+      data: JSON.parse(JSON.stringify(images)),
+      totalPage: Math.ceil(totalImages / limit),
+      savedImages,
+    }
+  } catch (error) {
+    handleError(error)
+  }
+}
+
+// GET IMAGES BY USER
+export async function getUserImages({
+  limit = 9,
+  page = 1,
+  userId,
+}: {
+  limit?: number;
+  page: number;
+  userId: string;
+}) {
+  try {
+    await connectToDatabase();
+
+    const skipAmount = (Number(page) - 1) * limit;
+
+    const images = await populateUser(Image.find({ author: userId }))
+      .sort({ updatedAt: -1 })
+      .skip(skipAmount)
+      .limit(limit);
+
+    const totalImages = await Image.find({ author: userId }).countDocuments();
+
+    return {
+      data: JSON.parse(JSON.stringify(images)),
+      totalPages: Math.ceil(totalImages / limit),
+    };
   } catch (error) {
     handleError(error);
   }
