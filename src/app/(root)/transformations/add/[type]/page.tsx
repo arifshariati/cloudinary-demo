a9b9b6edd422920f169f1c62cd440bd72d9c65bf
Arif Shariati <mohammad.arif.fast@hotmail.com>
2024-05-09 08:53:53 +0200

    added transformation page

--- src/app/(root)/transformations/add/[type]/page.tsx
+++ src/app/(root)/transformations/add/[type]/page.tsx
@@ -1,5 +1,25 @@
-const AddTransformationTypePage = () => {
-  return <div>AddTransformationTypePage</div>;
+import Header from "@/components/shared/Header";
+import TransformationForm from "@/components/shared/TransformationForm";
+import { transformationTypes } from "@/constants";
+import { getUserById } from "@/lib/actions/user.actions";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+const AddTransformationTypePage = async ({ params: { type } }: SearchParamProps) => {
+  const { title, subTitle, type: tType } = transformationTypes[type];
+  const { userId } = auth();
+
+  if (!userId) redirect("/sign-in");
+
+  const user = await getUserById(userId);
+  return (
+    <>
+      <Header title={title} subTitle={subTitle} />
+      <section>
+        <TransformationForm action="Add" userId={user._id} type={tType as TransformationTypeKey} creditBalance={user.creditBalance} />
+      </section>
+    </>
+  );
 };
 
 export default AddTransformationTypePage;
