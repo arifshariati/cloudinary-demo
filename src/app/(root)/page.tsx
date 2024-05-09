15bc2c7d880f7d0bccb1f8a43f6bef841d3ac3fa
Arif Shariati <mohammad.arif.fast@hotmail.com>
2024-05-09 12:53:17 +0200

    added homepage components

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,8 +1,30 @@
-const HomePage = () => {
+import { Collection } from "@/components/shared/Collection";
+import { navLinks } from "@/constants";
+import { getAllImages } from "@/lib/actions/image.actions";
+import Image from "next/image";
+import Link from "next/link";
+
+const HomePage = async ({ searchParams }: SearchParamProps) => {
+  const page = Number(searchParams?.page || 1);
+  const searchQuery = (searchParams?.query as string) || "";
+  const images = await getAllImages({ page, searchQuery });
   return (
     <>
       <section className="home">
-        <h1>Home Page</h1>
+        <h1 className="home-heading">Are you ready to test cloudinary 🚀</h1>
+        <ul className="flex-center w-full gap-20">
+          {navLinks.slice(1, 5).map(({ icon, route, label }) => (
+            <Link key={route} href={route} className="flex-center flex-col gap-2">
+              <li className="flex-center w-fit rounded-[10px] bg-white border-solid border-slate-200 border-2  p-4">
+                <Image src={icon} alt={`${label} icon`} width={24} height={24} />
+              </li>
+              <p className="p-14-medium text-center">{label}</p>
+            </Link>
+          ))}
+        </ul>
+      </section>
+      <section className="sm:mt-12">
+        <Collection hasSearch={true} images={images?.data} totalPages={images?.totalPage} page={page} />
       </section>
     </>
   );
